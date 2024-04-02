3dc6561664ebbe7ee5cb3e1ae888b3474cbfadb0
Reid Herrera <reidherrerabusiness@gmail.com>
2024-04-01 20:09:14 -0500

    added the hidden class as well as gave it functionality within the modal. also adjusted modal

--- assets/js/landingpage.js
+++ assets/js/landingpage.js
@@ -1,4 +1,14 @@
-document.getElementById('open-modal').addEventListener('click', function() {
-  var modal = document.getElementById('modal');
-  modal.style.display = 'block'; 
+document.getElementById("open-modal").addEventListener("click", function () {
+  var modal = document.getElementById("modal");
+  modal.style.display = "block";
+});
+
+document.getElementById("close-modal").addEventListener("click", function () {
+  var modal = document.getElementById("modal");
+  modal.style.display = "none";
+});
+
+document.getElementById("close-modal2").addEventListener("click", function () {
+  var modal = document.getElementById("modal");
+  modal.style.display = "none";
 });
