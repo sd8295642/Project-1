2518905426c5696829276771968f375e6236cf44
sd8295642 <sd8295642@gmail.com>
2024-04-04 21:37:57 -0500

    Added javascript functions to handle the API call and display the response data

--- assets/js/landingpage.js
+++ assets/js/landingpage.js
@@ -1,3 +1,57 @@
+
+
+// const locationArray = [document.querySelector('#austin'), 'Houston', 'Dallas', 'San&Antonio']
+
+// function getLocation(locationArray){
+  
+//   for(i = 0; i < locationArray.length; i++){
+//     let location = locationArray[i]
+//   }
+//   return location;
+// }
+
+async function getMinMaxTemp(location){
+  const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+const response = await fetch(`https://api.tomorrow.io/v4/weather/forecast?location=${location}&apikey=rhJyv4LoiHg6oPHSEP9vvI1YYzNsO1UR`, options)
+  const data = await response.json()
+
+    console.log(data)
+    let min = data.timelines.daily[0].values.temperatureMin
+    let max = data.timelines.daily[0].values.temperatureMax
+
+    min = celcToFahr(min)
+    max = celcToFahr(max)
+
+    console.log(min, max)
+    return {min, max}
+}
+
+function celcToFahr( n ) {
+  return ((n * 9.0 / 5.0) + 32.0);
+}
+
+async function displayMinMaxTemp(min, max) {
+  const htmlTempArray = document.querySelectorAll('.temp-display')
+  
+  console.log(htmlTempArray)
+  for (let i = 0; i < htmlTempArray.length; i++) {
+    const location = htmlTempArray[i].dataset.location
+    console.log(location)
+    const data = await getMinMaxTemp(location);
+    console.log(data)
+
+    const temperatureDisplay = `
+    <p>Temperature Range: ${data.min} to ${data.max}</p>
+    `;
+    htmlTempArray[i].innerHTML = temperatureDisplay
+    
+  }
+}
+
+displayMinMaxTemp();
+
+
 document.getElementById("open-modal").addEventListener("click", function () {
   var modal = document.getElementById("modal");
   modal.style.display = "block";
@@ -12,3 +66,4 @@ document.getElementById("close-modal2").addEventListener("click", function () {
   var modal = document.getElementById("modal");
   modal.style.display = "none";
 });
+
